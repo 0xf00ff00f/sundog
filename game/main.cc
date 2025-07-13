e657a98c5bc07254463cda2a53702ffe30fb7a90
0xf00ff00f <fzorto@gmail.com>
2025-07-12 22:23:06 -0300

    Add Mesh class

--- game/main.cc
+++ game/main.cc
@@ -1,5 +1,7 @@
 #include <glad/gl.h>
+
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 
 #include <cassert>
@@ -7,6 +9,73 @@
 
 import glhelpers;
 import std;
+
+class Mesh
+{
+public:
+    Mesh();
+    ~Mesh();
+
+    Mesh(Mesh &&other);
+    Mesh &operator=(Mesh &&other);
+
+    Mesh(const Mesh &) = delete;
+    Mesh &operator=(const Mesh &) = delete;
+
+    enum class Type : GLenum
+    {
+        Float = GL_FLOAT
+    };
+    struct VertexAttribute
+    {
+        std::size_t size;
+        Type type;
+        std::size_t offset;
+    };
+    void setVertexAttributes(std::span<const VertexAttribute> attributes, std::size_t stride);
+    void setVertexData(std::span<const std::byte> vertexData);
+    void draw(std::size_t firstVertex, std::size_t vertexCount);
+
+private:
+    gl::Buffer m_vertexBuffer;
+    gl::VertexArray m_vertexArray;
+};
+
+Mesh::Mesh()
+    : m_vertexBuffer(gl::Buffer::Target::ArrayBuffer)
+{
+}
+
+Mesh::~Mesh() = default;
+
+Mesh::Mesh(Mesh &&other) = default;
+
+Mesh &Mesh::operator=(Mesh &&other) = default;
+
+void Mesh::setVertexAttributes(std::span<const VertexAttribute> attributes, std::size_t stride)
+{
+    m_vertexArray.bind();
+    m_vertexBuffer.bind();
+    // where's std::views::enumerate?
+    for (const auto &&[index, attribute] : std::views::zip(std::views::iota(0), attributes))
+    {
+        glEnableVertexAttribArray(index);
+        glVertexAttribPointer(index, attribute.size, static_cast<GLenum>(attribute.type), GL_FALSE, stride,
+                              reinterpret_cast<const void *>(attribute.offset));
+    }
+}
+
+void Mesh::setVertexData(std::span<const std::byte> vertexData)
+{
+    m_vertexBuffer.bind();
+    m_vertexBuffer.data(vertexData, gl::Buffer::Usage::StaticDraw);
+}
+
+void Mesh::draw(std::size_t firstVertex, std::size_t vertexCount)
+{
+    m_vertexArray.bind();
+    glDrawArrays(GL_TRIANGLES, firstVertex, vertexCount);
+}
 
 int main(int argc, char *argv[])
 {
@@ -43,27 +112,43 @@ int main(int argc, char *argv[])
     {
         using namespace gl;
 
-        std::vector<float> points = {0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
-        Buffer buffer(Buffer::Target::ArrayBuffer);
-        buffer.bind();
-        buffer.data(std::as_bytes(std::span{points}), Buffer::Usage::StaticDraw);
+        struct Vertex
+        {
+            glm::vec3 position;
+            glm::vec3 color;
+        };
+        const std::array<Vertex, 3> vertices = {
+            Vertex{.position = glm::vec3{0.0f, 0.5f, 0.0f}, .color = glm::vec3{1.0, 0.0, 0.0}},
+            Vertex{.position = glm::vec3{0.5f, -0.5f, 0.0f}, .color = glm::vec3{0.0, 1.0, 0.0}},
+            Vertex{.position = glm::vec3{-0.5f, -0.5f, 0.0f}, .color = glm::vec3{0.0, 0.0, 1.0}}};
 
-        VertexArray vao;
-        vao.bind();
-        glEnableVertexAttribArray(0);
-        buffer.bind();
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        const std::array<Mesh::VertexAttribute, 2> attributes = {
+            Mesh::VertexAttribute{3, Mesh::Type::Float, offsetof(Vertex, position)},
+            Mesh::VertexAttribute{3, Mesh::Type::Float, offsetof(Vertex, color)},
+        };
+
+        Mesh mesh;
+        mesh.setVertexData(std::as_bytes(std::span{vertices}));
+        mesh.setVertexAttributes(attributes, sizeof(Vertex));
 
         const char *vertexShader = R"(
-in vec3 position;
+layout(location=0) in vec3 position;
+layout(location=1) in vec3 color;
+
+out vec3 vs_color;
+
 void main() {
+    vs_color = color;
     gl_Position = vec4(position, 1.0);
 }
 )";
         const char *fragmentShader = R"(
+in vec3 vs_color;
+
 out vec4 fragColor;
+
 void main() {
-    fragColor = vec4(1.0);
+    fragColor = vec4(vs_color, 1.0);
 }
 )";
         ShaderProgram program;
@@ -91,9 +176,7 @@ void main() {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             program.use();
-            vao.bind();
-
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            mesh.draw(0, 3);
 
             glfwSwapBuffers(window);
         }
