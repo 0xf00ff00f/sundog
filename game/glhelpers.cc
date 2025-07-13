0abf01f5ea5257bf38f574467e0b056f616f4970
0xf00ff00f <fzorto@gmail.com>
2025-07-12 21:24:00 -0300

    Add basic OpenGL helpers

---
+++ game/glhelpers.cc
@@ -0,0 +1,250 @@
+module;
+
+#include <glad/gl.h>
+
+#include <print>
+#include <ranges>
+#include <span>
+#include <string_view>
+#include <vector>
+
+export module glhelpers;
+
+export namespace gl
+{
+
+class Buffer
+{
+public:
+    enum class Target : GLenum
+    {
+        ArrayBuffer = GL_ARRAY_BUFFER
+    };
+
+    enum class Usage
+    {
+        StaticDraw = GL_STATIC_DRAW
+    };
+
+    explicit Buffer(Target target);
+    ~Buffer();
+
+    Buffer(Buffer &&other);
+    Buffer &operator=(Buffer &&other);
+
+    Buffer(const Buffer &) = delete;
+    Buffer &operator=(const Buffer &) = delete;
+
+    GLuint handle() const { return m_handle; }
+
+    void bind() const;
+    void data(std::span<const std::byte> bytes, Usage usage);
+
+private:
+    Target m_target;
+    GLuint m_handle = 0;
+};
+
+class VertexArray
+{
+public:
+    VertexArray();
+    ~VertexArray();
+
+    VertexArray(VertexArray &&other);
+    VertexArray &operator=(VertexArray &&other);
+
+    VertexArray(const VertexArray &) = delete;
+    VertexArray &operator=(const VertexArray &) = delete;
+
+    GLuint handle() const { return m_handle; }
+
+    void bind() const;
+
+private:
+    GLuint m_handle = 0;
+};
+
+class ShaderProgram
+{
+public:
+    ShaderProgram();
+    ~ShaderProgram();
+
+    ShaderProgram(ShaderProgram &&other);
+    ShaderProgram &operator=(ShaderProgram &&other);
+
+    ShaderProgram(const ShaderProgram &) = delete;
+    ShaderProgram &operator=(const ShaderProgram &) = delete;
+
+    GLuint handle() const { return m_handle; }
+
+    enum class ShaderType : GLenum
+    {
+        VertexShader = GL_VERTEX_SHADER,
+        FragmentShader = GL_FRAGMENT_SHADER
+    };
+
+    bool attachShader(ShaderType type, std::string_view source) const;
+    bool link() const;
+    void use() const;
+
+private:
+    GLuint m_handle = 0;
+};
+
+Buffer::Buffer(Target target)
+    : m_target(target)
+{
+    glGenBuffers(1, &m_handle);
+}
+
+Buffer::~Buffer()
+{
+    if (m_handle)
+        glDeleteBuffers(1, &m_handle);
+}
+
+Buffer::Buffer(Buffer &&other)
+    : m_handle(std::exchange(other.m_handle, 0))
+{
+}
+
+Buffer &Buffer::operator=(Buffer &&other)
+{
+    if (this != &other)
+    {
+        if (m_handle)
+            glDeleteBuffers(1, &m_handle);
+        m_handle = std::exchange(other.m_handle, 0);
+    }
+    return *this;
+}
+
+void Buffer::bind() const
+{
+    glBindBuffer(static_cast<GLenum>(m_target), m_handle);
+}
+
+void Buffer::data(std::span<const std::byte> bytes, Usage usage)
+{
+    glBufferData(static_cast<GLenum>(m_target), bytes.size(), bytes.data(), static_cast<GLenum>(usage));
+}
+
+VertexArray::VertexArray()
+{
+    glGenVertexArrays(1, &m_handle);
+}
+
+VertexArray::~VertexArray()
+{
+    if (m_handle)
+        glDeleteVertexArrays(1, &m_handle);
+}
+
+VertexArray::VertexArray(VertexArray &&other)
+    : m_handle(std::exchange(other.m_handle, 0))
+{
+}
+
+VertexArray &VertexArray::operator=(VertexArray &&other)
+{
+    if (this != &other)
+    {
+        if (m_handle)
+            glDeleteVertexArrays(1, &m_handle);
+        m_handle = std::exchange(other.m_handle, 0);
+    }
+    return *this;
+}
+
+void VertexArray::bind() const
+{
+    glBindVertexArray(m_handle);
+}
+
+ShaderProgram::ShaderProgram()
+    : m_handle(glCreateProgram())
+{
+}
+
+ShaderProgram::~ShaderProgram()
+{
+    glDeleteProgram(m_handle);
+}
+
+ShaderProgram::ShaderProgram(ShaderProgram &&other)
+    : m_handle(std::exchange(other.m_handle, 0))
+{
+}
+
+ShaderProgram &ShaderProgram::operator=(ShaderProgram &&other)
+{
+    if (this != &other)
+    {
+        if (m_handle)
+            glDeleteProgram(m_handle);
+        m_handle = std::exchange(other.m_handle, 0);
+    }
+    return *this;
+}
+
+bool ShaderProgram::attachShader(ShaderType type, std::string_view source) const
+{
+    GLuint shader = glCreateShader(static_cast<GLenum>(type));
+
+    std::array<std::string_view, 2> sources = {
+        "#version 410 core\n",
+        source,
+    };
+    // clang-format off
+    const std::vector<const GLchar *> strings = sources
+        | std::views::transform([](std::string_view s) { return reinterpret_cast<const GLchar *>(s.data()); })
+        | std::ranges::to<std::vector>();
+    const std::vector<GLint> lengths = sources
+        | std::views::transform([](std::string_view s) { return static_cast<GLint>(s.size()); })
+        | std::ranges::to<std::vector>();
+    // clang-format on
+    glShaderSource(shader, sources.size(), strings.data(), lengths.data());
+    glCompileShader(shader);
+
+    GLint status = GL_FALSE;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        std::array<char, 1024> log;
+        GLsizei length = 0;
+        glGetShaderInfoLog(shader, log.size(), &length, log.data());
+        std::println(stderr, "Failed to compile shader: {}", std::string_view(log.data(), length));
+        return false;
+    }
+
+    glAttachShader(m_handle, shader);
+
+    return true;
+}
+
+bool ShaderProgram::link() const
+{
+    glLinkProgram(m_handle);
+
+    GLint status = GL_FALSE;
+    glGetProgramiv(m_handle, GL_LINK_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        std::array<char, 1024> log;
+        GLsizei length;
+        glGetProgramInfoLog(m_handle, log.size(), &length, log.data());
+        std::println(stderr, "Failed to link program: {}", std::string_view(log.data(), length));
+        return false;
+    }
+
+    return true;
+}
+
+void ShaderProgram::use() const
+{
+    glUseProgram(m_handle);
+}
+
+} // namespace gl
--- game/main.cc
+++ game/main.cc
@@ -1,13 +1,17 @@
 #include <glad/gl.h>
+
 #include <GLFW/glfw3.h>
 
+#include <cassert>
 #include <stdio.h>
 
+import glhelpers;
 import std;
 
 int main(int argc, char *argv[])
 {
-    if (!glfwInit()) {
+    if (!glfwInit())
+    {
         std::println(stderr, "Failed to initialize GLFW");
         return 1;
     }
@@ -17,7 +21,8 @@ int main(int argc, char *argv[])
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *window = glfwCreateWindow(400, 400, "Hello", nullptr, nullptr);
-    if (!window) {
+    if (!window)
+    {
         std::println(stderr, "Failed to create window");
         glfwTerminate();
         return 1;
@@ -25,7 +30,8 @@ int main(int argc, char *argv[])
     glfwMakeContextCurrent(window);
 
     const auto gladVersion = gladLoadGL(glfwGetProcAddress);
-    if (gladVersion == 0) {
+    if (gladVersion == 0)
+    {
         std::println(stderr, "Failed to initialize OpenGL context");
         glfwTerminate();
         return 1;
@@ -34,12 +40,63 @@ int main(int argc, char *argv[])
     std::println("OpenGL renderer: {}", reinterpret_cast<const char *>(glGetString(GL_RENDERER)));
     std::println("OpenGL version: {}", reinterpret_cast<const char *>(glGetString(GL_VERSION)));
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    {
+        using namespace gl;
 
-        // TODO
+        std::vector<float> points = {0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
+        Buffer buffer(Buffer::Target::ArrayBuffer);
+        buffer.bind();
+        buffer.data(std::as_bytes(std::span{points}), Buffer::Usage::StaticDraw);
 
-        glfwSwapBuffers(window);
+        VertexArray vao;
+        vao.bind();
+        glEnableVertexAttribArray(0);
+        buffer.bind();
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        const char *vertexShader = R"(
+in vec3 position;
+void main() {
+    gl_Position = vec4(position, 1.0);
+}
+)";
+        const char *fragmentShader = R"(
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(1.0);
+}
+)";
+        ShaderProgram program;
+        if (!program.attachShader(ShaderProgram::ShaderType::FragmentShader, fragmentShader))
+        {
+            glfwTerminate();
+            return 1;
+        }
+        if (!program.attachShader(ShaderProgram::ShaderType::VertexShader, vertexShader))
+        {
+            glfwTerminate();
+            return 1;
+        }
+        if (!program.link())
+        {
+            glfwTerminate();
+            return 1;
+        }
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+
+            glClearColor(1.0, 0.0, 0.0, 1.0);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            program.use();
+            vao.bind();
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            glfwSwapBuffers(window);
+        }
     }
 
     glfwTerminate();
